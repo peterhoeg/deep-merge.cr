bd79f63cc5d4d4bd22d3b8b6661070c648b764bf
Peter Hoeg <peter@hoeg.com>
2020-05-14 21:09:37 +0800

    wip

--- spec/deep-merge_spec.cr
+++ spec/deep-merge_spec.cr
@@ -1,135 +1,165 @@
 require "./spec_helper"
 
 describe Hash do
-  it "merges 2 basic hashes" do
-    a = {
-      "a" => 1,
-    }
+  describe "merges hashes of the same type" do
+    describe "basic hashes" do
+      it "merges values for non-overlapping keys" do
+        a = {
+          "a" => 1,
+        }
 
-    b = {
-      "b" => 2,
-    }
+        b = {
+          "b" => 2,
+        }
 
-    result = {
-      "a" => 1,
-      "b" => 2,
-    }
+        result = {
+          "a" => 1,
+          "b" => 2,
+        }
 
-    a.deep_merge(b).should eq(result)
+        a.deep_merge(b).should eq(result)
+        a.deep_merge!(b).should eq(result)
+      end
+
+      it "overwrites values for overlapping keys" do
+        a = {
+          "a" => 1,
+        }
+
+        b = {
+          "a" => 2,
+        }
+
+        result = {
+          "a" => 2,
+        }
+
+        a.deep_merge(b).should eq(result)
+        a.deep_merge!(b).should eq(result)
+      end
+    end
+
+    describe "nested hashes" do
+      it "overwrites values for overlapping keys" do
+        a = {
+          "a" => {
+            "b" => 2,
+          },
+        }
+
+        b = {
+          "a" => {
+            "b" => 3,
+          },
+        }
+
+        result = {
+          "a" => {
+            "b" => 3,
+          },
+        }
+
+        a.deep_merge(b).should eq(result)
+        a.deep_merge!(b).should eq(result)
+      end
+
+      it "overwrites values overlapping keys without children when nested and preserves siblings" do
+        a = {
+          "a" => {
+            "b" => 2,
+            "c" => 2,
+          },
+        }
+
+        b = {
+          "a" => {
+            "c" => 3,
+          },
+          "d" => 4,
+        }
+
+        result = {
+          "a" => {
+            "b" => 2,
+            "c" => 3,
+          },
+          "d" => 4,
+        }
+
+        a.deep_merge(b).should eq(result)
+      end
+    end
+
+    it "nested hashes" do
+      a = {
+        "a" => {
+          "b" => 2,
+        },
+      }
+
+      b = {
+        "a" => {
+          "c" => 3,
+        },
+        "d" => 4,
+      }
+
+      result = {
+        "a" => {
+          "b" => 2,
+          "c" => 3,
+        },
+        "d" => 4,
+      }
+      a.deep_merge(b).should eq(result)
+    end
   end
 
-  it "merges nested hashes" do
-    a = {
-      "a" => {
-        "b" => 2,
-      },
-    }
+  describe "merges hashes of the different types" do
+    it "left is a nested hash" do
+      a = {
+        "a" => {
+          "b" => 2,
+        },
+      }
 
-    b = {
-      "c" => 3,
-    }
-
-    result = {
-      "a" => {
-        "b" => 2,
-      },
-      "c" => 3,
-    }
-
-    a.deep_merge(b).should eq(result)
-  end
-
-  it "merges hashes" do
-    a = {
-      "a" => {
-        "b" => 2,
-      },
-    }
-
-    b = {
-      "a" => {
+      b = {
         "c" => 3,
-      },
-      "d" => 4,
-    }
+      }
 
-    result = {
-      "a" => {
-        "b" => 2,
+      result = {
+        "a" => {
+          "b" => 2,
+        },
         "c" => 3,
-      },
-      "d" => 4,
-    }
-    a.deep_merge(b).should eq(result)
-  end
+      }
 
-  it "overwrites keys with same name if not a hash" do
-    a = {
-      "a" => 1,
-    }
+      a.deep_merge(b).should eq(result)
+    end
 
-    b = {
-      "a" => 2,
-    }
-
-    result = {
-      "a" => 2,
-    }
-
-    a.deep_merge(b).should eq(result)
-  end
-
-  it "overwrites keys with same name without children when nested" do
-    a = {
-      "a" => {
-        "b" => 2,
-      },
-    }
-
-    b = {
-      "a" => {
-        "b" => 3,
-      },
-      "d" => 4,
-    }
-
-    result = {
-      "a" => {
-        "b" => 3,
-      },
-      "d" => 4,
-    }
-
-    a.deep_merge(b).should eq(result)
-  end
-
-  it "overwrites keys with same name without children when nested but preserves siblings" do
-    a = {
-      "a" => {
-        "b" => 2,
-        "c" => 2,
-      },
-    }
-
-    b = {
-      "a" => {
+    it "right is a nested hash" do
+      a = {
         "c" => 3,
-      },
-      "d" => 4,
-    }
+      }
 
-    result = {
-      "a" => {
-        "b" => 2,
+      b = {
+        "a" => {
+          "b" => 2,
+        },
+      }
+
+      result = {
+        "a" => {
+          "b" => 2,
+        },
         "c" => 3,
-      },
-      "d" => 4,
-    }
+      }
 
-    a.deep_merge(b).should eq(result)
+      a.deep_merge(b).should eq(result)
+    end
   end
 
-  it "overwrites keys with same name if only one is a hash" do
+  it "overwrites values for overlapping keys when right is a nested hash" do
     a = {
       "a" => 1,
     }
@@ -147,23 +177,6 @@ describe Hash do
     }
 
     a.deep_merge(b).should eq(result)
-  end
-
-  it "bang merges 2 basic hashes of the same type" do
-    a = {
-      "a" => 1,
-    }
-
-    b = {
-      "b" => 2,
-    }
-
-    result = {
-      "a" => 1,
-      "b" => 2,
-    }
-
-    a.deep_merge!(b).should eq(result)
   end
 
   it "bang merges nested hashes of the same type" do
@@ -254,5 +267,4 @@ describe Hash do
 
     a.deep_merge!(b).should eq(result)
   end
-
 end
--- src/deep-merge.cr
+++ src/deep-merge.cr
@@ -1,4 +1,6 @@
 class Hash(K, V)
+  # Merges *other* and self and returns a new Hash with the combined types of
+  # *other* and self
   def deep_merge(other : Hash(L, W)) forall L, W
     target = Hash(K | L, V | W).new
     target.merge! self
@@ -6,6 +8,7 @@ class Hash(K, V)
     target.deep_merge! other
   end
 
+  # Merges *other* into self
   def deep_merge!(other : Hash(L, W)) forall L, W
     other.keys.each do |key|
       # keys only in other
